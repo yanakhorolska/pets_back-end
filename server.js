bd82d7ea9408c41b75c66bef26d34c7615f947bf
yanakhorolska <yana.khorolska@protonmail.com>
2023-02-15 11:12:16 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const { mongoose } = require("mongoose");
+const app = require("./app");
+mongoose.set("strictQuery", false);
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { MONGO_URI } = process.env;
+
+(async function main() {
+  try {
+    await mongoose
+      .connect(MONGO_URI)
+      .then(() => {
+        console.log("Database connection successful");
+        app.listen(3000, () => {
+          console.log("Server is running on port: 3000");
+        });
+      })
+      .catch((err) => {
+        console.error("Error with database connection:", err.message);
+        process.exit(1);
+      });
+  } catch (err) {
+    console.error("error:", err.message);
+  }
+})();
