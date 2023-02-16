843ee673fefce04f5c1b0cdd3f6687c7680b5dbb
Aleksandor2138 <99901672+Aleksandor2138@users.noreply.github.com>
2023-02-16 13:43:33 +0200

    json-en
    Подключил  swagger и добавил базовый json

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const logger = require("morgan");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger/care-pets-en.json");
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.static("public"));
 
 app.use((req, res) => {
