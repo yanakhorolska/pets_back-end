4b2f19a7bb4df2bcaa8e69171ff9b4a6054f4afd
OlgaBorovik <jkz8686@gmail.com>
2023-02-16 18:09:00 +0200

    logout

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,3 +1,10 @@
-async function logout(req, res, next) {}
+const { User } = require("../../models/userModel");
+
+async function logout(req, res) {
+    
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: null });
+  return res.status(204).json();
+}
 
 module.exports = logout;
--- routes/api/authRouter.js
+++ routes/api/authRouter.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { auth: ctrl } = require("../../controllers");
 const { ctrlWrapper } = require("../../helpers");
+const { authentificate} = require("../../middlewares")
 
 const authRouter = express.Router();
 
 authRouter.post("/register", ctrlWrapper(ctrl.register));
 authRouter.post("/login", ctrlWrapper(ctrl.login))
-// authRouter.post("/logout", auth, logout)
+authRouter.post("/logout", authentificate, ctrlWrapper(ctrl.logout))
 
 module.exports = {
   authRouter,
