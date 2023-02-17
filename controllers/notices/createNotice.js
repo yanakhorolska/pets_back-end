fc081da7ce2cad7620d67d8cbf2a19e2cf020ce8
Konstantyn <k.buhantsev@gmail.com>
2023-02-17 17:01:25 +0200

    notices

--- controllers/notices/createNotice.js
+++ controllers/notices/createNotice.js
@@ -2,8 +2,13 @@ const { Notice } = require("../../models/noticeModel");
 const { InternalServerError } = require("http-errors");
 
 const createNotice = async (req, res) => {
-  const user = req?.user;
-  const notice = await Notice.create({ ...req.body, owner: user?._id });
+  const user = req.user;
+  const { category } = req.params;
+  const notice = await Notice.create({
+    ...req.body,
+    category,
+    owner: user._id,
+  });
   if (notice) {
     res.status(201).json(notice);
   } else {
