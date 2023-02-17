56d831c5d03c89895d0f51c48eeb8337e905867c
PavelBakhtin <pavelbakhtin88@gmail.com>
2023-02-17 15:05:27 +0200

    1
    Зробив backend для роуту news.
    Реалізував пошук по title або description.

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const authRouter = require("./routes/api/authRoutes");
 const userRouter = require("./routes/api/userRoutes");
 // const petsRouter = require('./routes/api/petsRoutes')
 const noticesRouter = require("./routes/api/noticesRoutes");
-// const newsRouter = require("./routes/api/newsRoutes");
-//const sponsorsRouter = require('./routes/api/sponsorsRoutes');
-const servicesRouter = require('./routes/api/servicesRoutes');
+const newsRouter = require("./routes/api/newsRoutes");
+// const sponsorsRouter = require('./routes/api/sponsorsRoutes');
+const servicesRouter = require("./routes/api/servicesRoutes");
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
@@ -26,7 +26,7 @@ app.use("/api/users", userRouter);
 // app.use("/api/pets", petsRouter)
 app.use("/api/notices", noticesRouter);
 
-// app.use("/api/news", newsRouter);
+app.use("/api/news", newsRouter);
 app.use("/api/friends", servicesRouter);
 
 // app.use("/api/sponsor", sponsorsRouter)
