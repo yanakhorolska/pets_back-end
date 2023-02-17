0d5afcc06683ecfe3b1cd802ac6b8458c839c255
tarsweb <tarsweb@ukr.net>
2023-02-17 16:14:09 +0200

    fix bugs

--- controllers/user/current.js
+++ controllers/user/current.js
@@ -1,5 +1,5 @@
 const { User } = require('../../models/userModel');
-const { Pet }  = require('../../models/petModel')
+// const { Pet }  = require('../../models/petModel')
 
 const current = async (req, res) => {
     const {name, birthday, region, city, email, friend} = req.user;
