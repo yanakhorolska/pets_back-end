187054c0c65a7c1b104d95a940417e32dfffe4d9
Konstantyn <k.buhantsev@gmail.com>
2023-02-17 15:07:23 +0200

    create notice

--- controllers/notices/index.js
+++ controllers/notices/index.js
@@ -1,7 +1,9 @@
 const getAllNotices = require("./getAllNotices");
 const getNoticeById = require("./getNiticeById");
+const createNotice = require("./createNotice");
 
 module.exports = {
   getAllNotices,
   getNoticeById,
+  createNotice,
 };
--- routes/api/noticesRoutes.js
+++ routes/api/noticesRoutes.js
@@ -2,11 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const { notices: ctrl } = require("../../controllers");
-
 const { ctrlWrapper } = require("../../helpers");
 
-router.get("/", ctrlWrapper(ctrl.getAllNotices));
+const { addNoticeSchema } = require("../../models/noticeModel");
+const { validation, isValidId } = require("../../middlewares");
 
-router.get("/:noticeId", ctrlWrapper(ctrl.getNoticeById));
+router
+  .get("/", ctrlWrapper(ctrl.getAllNotices))
+  .post("/", validation(addNoticeSchema), ctrlWrapper(ctrl.createNotice));
+
+router //
+  .get("/:noticeId", isValidId, ctrlWrapper(ctrl.getNoticeById));
 
 module.exports = router;
