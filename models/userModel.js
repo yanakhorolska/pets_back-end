f2b2d019b1491e87a2ea91e43cc6f2f714ddec13
tarsweb <58361241+tarsweb@users.noreply.github.com>
2023-02-19 05:05:58 +0200

    add user

--- models/userModel.js
+++ models/userModel.js
@@ -84,8 +84,8 @@ const registerSchema = Joi.object({
   email: Joi.string().email({ tlds: false }).required(),
   password: Joi.string().alphanum().min(7).max(32).required(),
   confirmPassword: Joi.ref('password'),
-  name: Joi.string().min(3).required().pattern(/[A-Za-z]+/),
-  city: Joi.string().pattern(/[A-Za-z]+, [A-Za-z]+/),
+  name: Joi.string().min(3).pattern(/[A-Za-zА-Яа-я]+/).required(),
+  city: Joi.string().pattern(/[A-Za-zА-Яа-я]+, [A-Za-zА-Яа-я]+/),
   phone: Joi.string().pattern(/^\+380\d{9}$/),
 }).required()
 
@@ -95,11 +95,11 @@ const loginSchema = Joi.object({
 }).required()
 
 const updateSchema = Joi.object({
-  name: Joi.string().min(1).pattern(/[A-Za-z]+/),
+  name: Joi.string().min(1).pattern(/[A-Za-zА-Яа-я]+/),
   email: Joi.string().email({ tlds: false }).min(1),
   birthday: Joi.date(),
   phone: Joi.string().pattern(/^\+380\d{9}$/),
-  city: Joi.string().pattern(/[A-Za-z]+, [A-Za-z]+/),
+  city: Joi.string().pattern(/[[A-Za-zА-Яа-я]]+, [[A-Za-zА-Яа-я]]+/),
 }).required()
 
 const schemas = { registerSchema, loginSchema, updateSchema }
