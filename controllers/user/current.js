4506e337dddf814a1b974dc1ae3d527c36510b44
tarsweb <58361241+tarsweb@users.noreply.github.com>
2023-02-19 23:13:09 +0200

    refactoring

--- controllers/user/current.js
+++ controllers/user/current.js
@@ -1,8 +1,8 @@
 const { User } = require('../../models/userModel');
 
 const current = async (req, res) => {
-  const {name, birthday, email, city, phone, avatarUrl, friends} = req.user;
-  res.json({status: "sucsses", data: {name, birthday, email, city, phone ,avatarUrl, friends}})
+  const {name, birthday, email, city, phone, avatarUrl} = req.user;
+  res.json({status: "sucsses", data: {name, birthday, email, city, phone, avatarUrl}})
 }
 
 // #alternative
