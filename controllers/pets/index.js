375331a3fa84a93a8b6b8c92f32ba85584aa2d00
tarsweb <tarsweb@ukr.net>
2023-02-20 19:33:37 +0200

    fix bugs

--- controllers/pets/index.js
+++ controllers/pets/index.js
@@ -1,5 +1,5 @@
 const getAll = require('./getAll');
-const getById = require('./getById');
+//const getById = require('./getById');//
 
 const addPet = require('./addPet');
 const deletePet = require('./deletePet');
@@ -7,4 +7,4 @@ const deletePet = require('./deletePet');
 const updateById = require('./updateById');
 const updateAvatar = require('./avatar');
 
-module.exports = { getAll, getById, addPet, updateAvatar, updateById, deletePet }
+module.exports = { getAll, addPet, updateAvatar, updateById, deletePet }
