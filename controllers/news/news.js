0586d4998e0074d9c7bda4c93983bd0fb32a0eaa
PavelBakhtin <pavelbakhtin88@gmail.com>
2023-02-20 19:56:24 +0200

    1

--- controllers/news/news.js
+++ controllers/news/news.js
@@ -1,24 +1,41 @@
 const { News } = require("../../models/newsModel");
+const { BadRequest } = require("http-errors");
 
 async function getNews(req, res) {
-  const { query } = req.query;
+  const { limit = 6, page } = req.query;
+  const skip = (page - 1) * limit;
+  try {
+    const allNews = await News.find({});
+    const total = allNews.length;
+    const news = await News.find({}).sort({ date: -1 }).skip(skip).limit(limit);
+    const { query } = req.query;
+    if (query) {
+      const allNews = await News.find({
+        $or: [
+          { title: { $regex: query, $options: "i" } },
+          { description: { $regex: query, $options: "i" } },
+        ],
+      });
+      const total = allNews.length;
+      const news = await News.find({
+        $or: [
+          { title: { $regex: query, $options: "i" } },
+          { description: { $regex: query, $options: "i" } },
+        ],
+      })
+        .skip(skip)
+        .limit(limit);
+      if (news.length === 0) {
+        return res.status(200).json({ message: "No news found" });
+      }
 
-  if (query) {
-    const filteredNews = await News.find({
-      $or: [
-        { title: { $regex: query, $options: "i" } },
-        { description: { $regex: query, $options: "i" } },
-      ],
-    });
-    if (filteredNews.length === 0) {
-      res.json({status: "success", data: { message: "No news found" } });
+      return res.status(200).json({ news, total });
     }
-    res.json({status: "success", data: filteredNews});
+    return res.status(200).json({ news, total });
+  } catch (error) {
+    console.log(error);
+    throw BadRequest();
   }
-
-  const news = await News.find({});
-
-  res.json({status: "success", data: news});
 }
 
 module.exports = { getNews };
