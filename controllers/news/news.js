102a744b94c8054d9f6bb49b632b1656c0c8ec21
PavelBakhtin <pavelbakhtin88@gmail.com>
2023-02-20 17:07:28 +0200

    1
    Додав пагінацію

--- controllers/news/news.js
+++ controllers/news/news.js
@@ -2,22 +2,36 @@ const { News } = require("../../models/newsModel");
 const { BadRequest } = require("http-errors");
 
 async function getNews(req, res) {
+  const { limit = 6, page } = req.query;
+  const skip = (page - 1) * limit;
   try {
+    const allNews = await News.find({});
+    const total = allNews.length;
+    const news = await News.find({}).sort({ date: -1 }).skip(skip).limit(limit);
     const { query } = req.query;
     if (query) {
-      const filteredNews = await News.find({
+      const allNews = await News.find({
         $or: [
           { title: { $regex: query, $options: "i" } },
           { description: { $regex: query, $options: "i" } },
         ],
       });
-      if (filteredNews.length === 0) {
+      const total = allNews.length;
+      const news = await News.find({
+        $or: [
+          { title: { $regex: query, $options: "i" } },
+          { description: { $regex: query, $options: "i" } },
+        ],
+      })
+        .skip(skip)
+        .limit(limit);
+      if (news.length === 0) {
         return res.status(200).json({ message: "No news found" });
       }
-      return res.status(200).json(filteredNews);
+
+      return res.status(200).json({ news, total });
     }
-    const news = await News.find({});
-    return res.status(200).json(news);
+    return res.status(200).json({ news, total });
   } catch (error) {
     console.log(error);
     throw BadRequest();
