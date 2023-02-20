f6643e8139ea5285c2a850bea71c7d5682144e38
tarsweb <58361241+tarsweb@users.noreply.github.com>
2023-02-20 21:07:11 +0200

    refactoring

--- controllers/pets/index.js
+++ controllers/pets/index.js
@@ -1,10 +1,17 @@
-const getAll = require('./getAll');
-const getById = require('./getPetById');//
+const getAll = require("./getAll")
+const getById = require("./getPetById")
 
-const addPet = require('./addPet');
-const deletePet = require('./deletePet');
+const addPet = require("./addPet");
+const deletePet = require("./deletePet")
 
-const updateById = require('./updatePetById');
-const updateAvatar = require('./avatar');
+const updateById = require("./updatePetById")
+const updateAvatar = require("./avatar")
 
-module.exports = { getAll, getById, addPet, updateAvatar, updateById, deletePet }
+module.exports = {
+  getAll,
+  getById,
+  addPet,
+  updateAvatar,
+  updateById,
+  deletePet,
+}
