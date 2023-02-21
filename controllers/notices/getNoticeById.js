535ee1a05e23140d55b9aef0d6dde7ed53432c1f
tarsweb <tarsweb@ukr.net>
2023-02-21 15:34:42 +0200

    add checkUser

--- controllers/notices/getNoticeById.js
+++ controllers/notices/getNoticeById.js
@@ -1,16 +1,22 @@
-const { Notice } = require("../../models/noticeModel");
+const { Notice, FavoriteNotice } = require("../../models/noticeModel");
 const { NotFound } = require("http-errors");
 
 const getNoticeById = async (req, res) => {
   const { noticeId } = req.params;
-
-  const notice = await Notice.findById(noticeId, ).populate(
-    "owner",
-    "_id name email"
-  );
+  const user = req?.user;
+ 
+  const notice = await Notice.findById(noticeId, "-createdAt -updatedAt").populate("owner")
 
   if (notice) {
-    res.json({status: "success", data: notice});
+    const { owner, ...result}  = notice.toObject()
+    const { email = "", phone ="" } = owner
+    const addInformation = {email, phone}
+    if (user) {
+      if (await FavoriteNotice.findOne({user, notice}))
+        addInformation.favorite = true;
+      addInformation.favorite = false;
+    }
+    res.json({status: "success", data: {...result, ...addInformation}});
   } else {
     throw NotFound(`Can not find notice with ID:${noticeId}`);
   }
